9bf83f44e22b54d8b67f6d9b6ed4cc142d865d30
Hl3n <119808859+Hl3n@users.noreply.github.com>
2025-11-03 15:55:53 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hl3n/valorsite/refs/heads/main/redirects/wm.lua"))()
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c65ee2fb8f1b5b9798bd01934bea08f7.lua"))()
